dccb3526e1248ddf82ae76abbf282e9ba5f59a8c
Andrei Krayushkin <neroreflex1@gmail.com>
2021-01-22 22:00:05 +0300

    Task 4-2 contact Prerelease, no search, corrected one

--- task4-2contacts-kotlin/src/main/java/by/andrei/firstproject/task4contacts/AddContact.kt
+++ task4-2contacts-kotlin/src/main/java/by/andrei/firstproject/task4contacts/AddContact.kt
@@ -1,5 +1,6 @@
 package by.andrei.firstproject.task4contacts
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -38,10 +39,15 @@ class AddContact : AppCompatActivity() {
                 R.drawable.ic_baseline_contact_mail_24
             }
 
-            val intent = intent
-            contact = Contact(image, textName, textPhoneOrEmail)
+//            val intent = Intent(this, MainActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+//            contact = Contact(image, textName, textPhoneOrEmail)
+//            intent.putExtra("TEXT", contact)
 
-            intent.putExtra("TEXT", contact)
+            val intent = Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                putExtra("TEXT", Contact(image, textName, textPhoneOrEmail))
+            }
 
             setResult(RESULT_OK, intent)
             finish()
